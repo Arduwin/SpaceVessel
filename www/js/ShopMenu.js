bfd51a29f9253f62305aee53aad4f3d7c0a9c6a5
Jan Hügel <huegel.jan@gmx.net>
2016-05-02 15:19:47 +0200

    tried new shop system

--- www/js/ShopMenu.js
+++ www/js/ShopMenu.js
@@ -10,21 +10,48 @@ SpaceHipster.ShopMenu = function () {};
 SpaceHipster.ShopMenu.prototype = {
 
 
-    //
+    init: function () {
 
-    // Load images and sounds
-    preload: function () {
+        //Load the plugin
+        this.game.kineticScrolling = this.game.plugins.add(Phaser.Plugin.KineticScrolling);
 
+        this.game.kineticScrolling.configure({
+            kineticMovement: true,
+            timeConstantScroll: 325, //really mimic iOS
+            horizontalScroll: true,
+            verticalScroll: false,
+            horizontalWheel: true,
+            verticalWheel: false,
+            deltaWheel: 40
+        });
     },
-
-    // Setup the example
     create: function () {
-        this.game.stage.backgroundColor = 0x4488cc;
-        window.alert("Test");
-    },
 
-    // The update() method is called every frame
-    update: function () {
+        //Starts the plugin
+        this.game.kineticScrolling.start();
 
+        this.rectangles = [];
+
+        var initX = 50;
+
+        for (var i = 0; i < 26; i++) {
+            this.rectangles.push(this.createRectangle(initX, this.game.world.centerY - 100, 250, 200));
+            this.index = this.game.add.text(initX + 125, this.game.world.centerY, i + 1, {
+                font: 'bold 150px Arial',
+                align: "center"
+            });
+            this.index.anchor.set(0.5);
+            initX += 300;
+        }
+
+        //Changing the world width
+        this.game.world.setBounds(0, 0, 320 * this.rectangles.length, this.game.height);
     },
+    createRectangle: function (x, y, w, h) {
+        var sprite = this.game.add.graphics(x, y);
+        sprite.beginFill(Phaser.Color.getRandomColor(100, 255), 1);
+        sprite.bounds = new PIXI.Rectangle(0, 0, w, h);
+        sprite.drawRect(0, 0, w, h);
+        return sprite;
+    }
 };
