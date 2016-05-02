3829a054db325c404a664f6a8b3dfb08969fd13d
Jan Hügel <huegel.jan@gmx.net>
2016-05-02 16:10:09 +0200

    modified item creator

--- www/js/ShopMenu.js
+++ www/js/ShopMenu.js
@@ -17,7 +17,7 @@ SpaceHipster.ShopMenu.prototype = {
 
         this.game.kineticScrolling.configure({
             kineticMovement: true,
-            timeConstantScroll: 325, //really mimic iOS
+            timeConstantScroll: 50, //really mimic iOS
             horizontalScroll: true,
             verticalScroll: false,
             horizontalWheel: true,
@@ -34,14 +34,26 @@ SpaceHipster.ShopMenu.prototype = {
 
         var initX = 50;
 
-        for (var i = 0; i < 26; i++) {
-            this.rectangles.push(this.createRectangle(initX, this.game.world.centerY - 100, 250, 200));
-            this.index = this.game.add.text(initX + 125, this.game.world.centerY, i + 1, {
-                font: 'bold 150px Arial',
-                align: "center"
+        for (var i = 0; i < 10; i++) {
+            //this.rectangles.push(this.createRectangle(initX, this.game.world.centerY - 100, 250, 200));
+            var itemWidth = 100;
+            var itemHeight = 100;
+            var itemGap = 20;
+            this.rectangles.push(this.createRectangle(initX, (this.game.height - (this.game.height * 0.5)), itemWidth, itemHeight));
+            //this.rectangles.anchor.setTo(0.5, 0.5);
+
+            //this.index = this.game.add.text(initX + 125, (this.game.height - (this.game.height * 0.5)), i + 1, {
+            this.index = this.game.add.text(initX + (itemWidth / 2), (this.game.height - (this.game.height * 0.5)) + (itemHeight / 2), i + 1, {
+                font: 'bold 20px Arial',
+                fill: '#FF0000'
             });
-            this.index.anchor.set(0.5);
-            initX += 300;
+            this.index.anchor.setTo(0.5, 0.5);
+            //this.index.x = this.rectangles.x + (itemWidth / 2);
+            //this.index.y = this.rectangles.y + (itemHeight / 2);
+            if (i == 0) {
+                initx = 0;
+            }
+            initX = initX + itemWidth + itemGap;
         }
 
         //Changing the world width
